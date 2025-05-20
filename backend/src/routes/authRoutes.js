0821a39f421b373c53971cb2d1218f8215b4743b
annuu1 <mannusng876@gmail.com>
2025-05-20 18:05:03 +0530

    Add 'me' route to authRoutes for retrieving user information; implement error handling for user not found and server errors.

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -130,4 +130,19 @@ router.post('/reset-password', async (req, res) => {
   }
 });
 
+// me route
+router.get('/me', async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.status(200).json(user);
+  }
+  catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+})
+
+
 module.exports = router;
